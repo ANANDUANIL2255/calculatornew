1657b76f79dd7f0d3f31fade3937b71a0f2caf26
unknown <unnikuttan7706@gmail.com>
2023-07-17 18:49:08 +0530

    calcu

---
+++ newcal.js
@@ -0,0 +1,12 @@
+function display(n){
+    result.value+=n
+}
+function equal(){
+    result.value=eval(result.value)
+}
+function clearScreen(){
+    result.value=""
+}
+function delet(){
+    result.value=(result.value).slice(0,-1)
+}
